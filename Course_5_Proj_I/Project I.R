d7268a79ef91532fc0203f34ea72fde5f901eb49
Hramos <ramoshumbertoa6@gmail.com>
2020-09-13 16:07:31 -0400

    total daily steps

--- Course_5_Proj_I/Project I.R
+++ Course_5_Proj_I/Project I.R
@@ -1,6 +1,7 @@
 #Load data
 library(dplyr)
 library(data.table)
+library(magrittr)
 #install.packages("ggplot2")
 library(ggplot2)
 #install.packages("hrbrthemes")
@@ -10,8 +11,7 @@ path = "~/Project/R/Data Science/R/Projectos/R_projectos/repdata_data_activity/a
 
 data <- read.csv(path,header=TRUE,sep=",")
 summary(data)
-data <- data[- which(is.na(data$steps)),]
-summary(data)
+
 
 
 Measures <- data %>% select(date,steps) %>% group_by(date) %>%
@@ -34,3 +34,37 @@ intervals <- data %>% select(interval,steps) %>%  na.omit() %>%
   theme_ipsum() +
   theme(axis.text.x=element_text(angle=60, hjust=1))+
    ggtitle("mean-Steps")
+
+ 
+ intervals[which(intervals$steps == max (intervals$steps)),]
+ 
+ #IMPUTING MISSING VALUES
+ 
+ #CALCULATE AND REPORT THE TOTAL NUMBER OF MISSING VALUES IN THE DATASET
+ 
+missing <-sum(is.na(data))
+missing
+ 
+replacewithmean <- function(x)
+  replace(x, is.na(x), mean(x, na.rm=TRUE))
+
+meandata <- data %>% group_by(interval) %>%
+  mutate(steps = replacewithmean(steps))
+
+head(meandata)
+
+# Make 
+
+FullSummedDataByDay <- aggregate(meandata$steps, by=list(meandata$date), sum)
+
+names(FullSummedDataByDay)[1] ="date"
+names(FullSummedDataByDay)[2] ="totalsteps"
+head(FullSummedDataByDay,15)
+                                    
+
+summary(FullSummedDataByDay)    
+
+ggplot(FullSummedDataByDay, aes(x=totalsteps, fill = cut(x=totalsteps, 100)))+
+  geom_histogram(show.legend=FALSE)+
+  ggtitle("Total Daily Steps")
+ 
--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 ## Repository to Assignment Exploratory Data Analysis
 
-link[](https://github.com/Hramos93/R_projectos/tree/master/Projecto_II Project I )
+[Project II](https://github.com/Hramos93/R_projectos/tree/master/Projecto_II Project I )
