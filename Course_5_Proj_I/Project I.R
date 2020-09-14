e5c6fee16711df3206f4315faf2867ece7ec63ea
Hramos <ramoshumbertoa6@gmail.com>
2020-09-14 09:50:29 -0400

    Complete task

--- Course_5_Proj_I/Project I.R
+++ Course_5_Proj_I/Project I.R
@@ -1,32 +1,41 @@
-#Load data
+#Libraries
 library(dplyr)
 library(data.table)
 library(magrittr)
 #install.packages("ggplot2")
 library(ggplot2)
 #install.packages("hrbrthemes")
+library(tidyverse)
 library(hrbrthemes)
+#install.packages("viridis")
+library(viridis)
 
+#Load data
 path = "~/Project/R/Data Science/R/Projectos/R_projectos/repdata_data_activity/activity.csv"
-
 data <- read.csv(path,header=TRUE,sep=",")
+
 summary(data)
 
-
-
+#Table sumarry of steps  by day
 Measures <- data %>% select(date,steps) %>% group_by(date) %>%
-  summarize(stepsbyday = sum(steps)) %>% na.omit()
+  summarize(stepsbyday = sum(steps , na.rm=TRUE)) 
 
+summary(Measures)
+
+#plot steps frecuency steps by day
 ggplot(Measures, aes(x=stepsbyday, fill = cut(x=stepsbyday, 100)))+
   geom_histogram(show.legend=FALSE)+
   ggtitle("Frequency Steps")
 
+##########MEASURES##################3333
+meanPreNa <- round(mean(Measures$stepsbyday),digits = 2)
+medianPreNA <- round(median(Measures$stepsbyday),digits = 2)
 
-mean(Measures$stepsbyday)
-median(Measures$stepsbyday)
 
+#Tablw with frequency steps by interval
 intervals <- data %>% select(interval,steps) %>%  na.omit() %>%
   group_by(interval) %>% summarize(steps = mean(steps))
+
 
  ggplot(intervals, aes(x=interval, y=steps)) +
   geom_line( color="#69b3a2") + 
@@ -40,7 +49,7 @@ intervals <- data %>% select(interval,steps) %>%  na.omit() %>%
  
  #IMPUTING MISSING VALUES
  
- #CALCULATE AND REPORT THE TOTAL NUMBER OF MISSING VALUES IN THE DATASET
+ #WITH MEAN
  
 missing <-sum(is.na(data))
 missing
@@ -53,8 +62,7 @@ meandata <- data %>% group_by(interval) %>%
 
 head(meandata)
 
-# Make 
-
+#Plot frequency steps 
 FullSummedDataByDay <- aggregate(meandata$steps, by=list(meandata$date), sum)
 
 names(FullSummedDataByDay)[1] ="date"
@@ -67,4 +75,28 @@ summary(FullSummedDataByDay)
 ggplot(FullSummedDataByDay, aes(x=totalsteps, fill = cut(x=totalsteps, 100)))+
   geom_histogram(show.legend=FALSE)+
   ggtitle("Total Daily Steps")
- 
+
+
+#COMPUTE NEW MEASURES DATA 
+
+meanPost <- round(mean(FullSummedDataByDay$totalsteps), digits = 2)
+medianPost <- round(median(FullSummedDataByDay$totalsteps), digits = 2)
+
+
+
+Compare <- data.frame(mean = c(meanPreNa,meanPost),median = c(medianPreNA,medianPost))
+rownames(Compare) <- c("Pre NA ", "Post NA ")
+print(Compare)
+
+#Plot Compare by stage week
+
+meandata$date <- as.Date(meandata$date)
+meandata$weekday <- weekdays(meandata$date)
+meandata$weekend <- ifelse(meandata$weekday=="sábado" | meandata$weekday=="domingo", "Weekend", "Weekday" )
+
+meandataweekendweekday <- aggregate(meandata$steps , by= list(meandata$weekend, meandata$interval), na.omit(mean))
+names(meandataweekendweekday) <- c("weekend", "interval", "steps")
+
+ggplot(meandataweekendweekday, aes(x=interval, y=steps, color=weekend)) + geom_line()+
+  facet_grid(weekend ~.) + xlab("Interval") + ylab("Mean of Steps") +
+  ggtitle("Comparison of Average Number of Steps in Each Interval")
