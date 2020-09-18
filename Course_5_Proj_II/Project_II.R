b718e344bd574f4a77d1ee44cd0616d4eb86c96f
Hramos <ramoshumbertoa6@gmail.com>
2020-09-18 15:12:39 -0400

    Processing mising values

--- Course_5_Proj_II/Project_II.R
+++ Course_5_Proj_II/Project_II.R
@@ -24,18 +24,6 @@ data$BGN_DATE <- format(as.Date(data$BGN_DATE, "%m/%d/%Y %H :%M :%S"), "%Y")
 data$BGN_DATE = as.numeric(levels(data$BGN_DATE))[data$BGN_DATE]
 str(data)
 
-unique(data$PROPDMGEXP)
-
-data$PROPDMGEXP <- as.character(data$PROPDMGEXP)
-data$PROPDMGEXP <- gsub("B","9", data$PROPDMGEXP)
-data$PROPDMGEXP <- gsub("H|h","2", data$PROPDMGEXP)
-data$PROPDMGEXP <- gsub("K","3", data$PROPDMGEXP)
-data$PROPDMGEXP <- gsub("M|m","6", data$PROPDMGEXP)
-data$PROPDMGEXP <- gsub("B","9", data$PROPDMGEXP)
-data$PROPDMGEXP <- gsub("-|?|+","0", data$PROPDMGEXP)
-
-data$PROPDMGEXP <- as.numeric(data$PROPDMGEXP)
-
 summary(data)
 
 INJURIES <- aggregate(data$INJURIES, by = list(EVTYPE = data$EVTYPE), sum) 
@@ -66,3 +54,31 @@ p2 <- ggplot(FATALITIES[1:5, ], aes(x=reorder(EVTYPE, -totalFatal), y =totalFata
         legend.text = element_text(color = "red", size = 5))
 
 grid.arrange(p1, p2, ncol = 2, top = "Plot")
+
+#property damage
+
+unique(data$PROPDMGEXP)
+
+data$PROPDMGEXP <- as.character(data$PROPDMGEXP)
+data$PROPDMGEXP[is.na(data$PROPDMGEXP)] <- 0
+data$PROPDMGEXP[!grepl("H|K|M|B", data$PROPDMGEXP,ignore.case = TRUE)] <- 0
+data$PROPDMGEXP <- gsub("H|h","2", data$PROPDMGEXP)
+data$PROPDMGEXP <- gsub("K|k","3", data$PROPDMGEXP)
+data$PROPDMGEXP <- gsub("M|m","6", data$PROPDMGEXP)
+data$PROPDMGEXP <- gsub("B|b","9", data$PROPDMGEXP)
+data$PROPDMGEXP <- as.numeric(as.character(data$PROPDMGEXP))
+data$propertyDMG <- data$PROPDMG * 10^data$PROPDMGEXP
+
+unique(data$CROPDMGEXP)
+
+data$CROPDMGEXP <- as.character(data$CROPDMGEXP)
+data$CROPDMGEXP[is.na(data$CROPDMGEXP)] <- 0
+data$CROPDMGEXP[!grepl("H|K|M|B", data$CROPDMGEXP,ignore.case = TRUE)] <- 0
+data$CROPDMGEXP <- gsub("H|h","2", data$CROPDMGEXP)
+data$CROPDMGEXP <- gsub("K|k","3", data$CROPDMGEXP)
+data$CROPDMGEXP <- gsub("M|m","6", data$CROPDMGEXP)
+data$CROPDMGEXP <- gsub("B|b","9", data$CROPDMGEXP)
+data$CROPDMGEXP <- as.numeric(as.character(data$CROPDMGEXP))
+data$CROP.DMG <- data$CROPDMG * 10^data$CROPDMGEXP
+
+sort(table(data$propertyDMG), decreasing = TRUE)[1:10]
